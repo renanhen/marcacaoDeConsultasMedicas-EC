4b7562c437a1e1dfb6aa240dfcf655d6b017c8a0
renanhen <renan05082000@gmail.com>
2025-03-28 20:38:01 -0300

    Projeto_base

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}
+      >Engenharia da computação</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    fontSize: 60,
+    color: '#2F0E54FF',
+    fontWeight: 'bold',
+  }
+});
